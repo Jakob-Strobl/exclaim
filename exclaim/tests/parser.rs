062392c71300fe3e9a08d105a9f7ef1187cd37ed
jakob <Jakobpstrobl@gmail.com>
2021-04-06 21:02:09 -0400

    Updated test to match next() removal from Node trait

--- exclaim/tests/parser.rs
+++ exclaim/tests/parser.rs
@@ -15,7 +15,7 @@ pub fn parser_test_empty_input() {
 
 #[test]
 pub fn parser_test_string_literal() {
-    let expected = "AST:\nNode: [ Token: Token { kind: StringLiteral, lexeme: \"Hello, World!\", location: Location { line: 0, column: 0 } }, Next: None ]";
+    let expected = "AST:\nNode: [ Token: Token { kind: StringLiteral, lexeme: \"Hello, World!\", location: Location { line: 0, column: 0 } } ]\n";
     let input = "Hello, World!";
 
     let tokens = crate::run_lexer(input);
@@ -26,7 +26,7 @@ pub fn parser_test_string_literal() {
 
 #[test]
 pub fn parser_test_simple_write_block() {
-    let input = "{{ write! \"Hello!\" }}";
+    let input = "{{ \"Hello!\" }}";
 
     let tokens = crate::run_lexer(input);
     let ast = crate::run_parser(tokens);
