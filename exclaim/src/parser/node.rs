8c65bf25e9baad27a68c9757f43f1378c893a9da
jakob <Jakobpstrobl@gmail.com>
2021-04-06 20:58:16 -0400

    Removed next() from node trait
    I don't think its needed, at least right now. The components of the nodes will differ, and not every node should have a next () item, so I removed it.

--- exclaim/src/parser/node.rs
+++ exclaim/src/parser/node.rs
@@ -3,18 +3,17 @@ use crate::tokens::Token;
 
 pub trait Node {
     fn token(&self) -> &Token;
-    fn next(&self) -> &Option<Box<dyn Node>>;
+    // fn next(&self) -> &Option<Box<dyn Node>>;
 }
 
 impl fmt::Debug for dyn Node {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Node: [ Token: {:?}, Next: {:?} ]", self.token(), self.next())
+        write!(f, "Node: [ Token: {:?} ]", self.token())
     }
 }
 
 pub struct TextNode {
     token: Token,
-    next: Option<Box<dyn Node>>,
 }
 
 impl Node for TextNode {
@@ -22,27 +21,22 @@ impl Node for TextNode {
         &self.token
     }
 
-    fn next(&self) -> &Option<Box<dyn Node>> {
-        &self.next
-    }
+    // fn next(&self) -> &Option<Box<dyn Node>> {
+    //     &self.next
+    // }
 }
 
 impl TextNode {
     pub fn new(token: Token) -> TextNode {
         TextNode {
             token,
-            next: None
         }
-    }
-
-    pub fn set_next(&mut self, node: Box<dyn Node>) {
-        self.next = Some(node);
     }
 }
 
 pub struct BlockNode {
     token: Token,
-    next: Option<Box<dyn Node>>,
+    stmt: Option<Box<dyn Node>>,
 }
 
 
@@ -50,21 +44,17 @@ impl Node for BlockNode {
     fn token(&self) -> &Token {
         &self.token
     }
-
-    fn next(&self) -> &Option<Box<dyn Node>> {
-        &self.next
-    }
 }
 
 impl BlockNode {
     pub fn new(token: Token) -> BlockNode {
         BlockNode {
             token,
-            next: None
+            stmt: None
         }
     }
 
-    pub fn set_next(&mut self, node: Box<dyn Node>) {
-        self.next = Some(node);
+    pub fn set_stmt(&mut self, node: Box<dyn Node>) {
+        self.stmt = Some(node);
     }
 }
