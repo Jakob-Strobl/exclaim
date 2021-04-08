b67f165a7bcd55f4e908e3d1726ff392e9fa89ab
jakob <Jakobpstrobl@gmail.com>
2021-04-07 22:37:18 -0400

    Added test to parse end stmt {{!}}; renamed Parser Tests

--- exclaim/tests/parser.rs
+++ exclaim/tests/parser.rs
@@ -3,7 +3,7 @@ use std::fmt::format;
 use exclaim::*;
 
 #[test]
-pub fn parser_empty_input() {
+pub fn parse_empty_input() {
     let expected = "AST:\n";
     let input = "";
 
@@ -14,7 +14,7 @@ pub fn parser_empty_input() {
 }
 
 #[test]
-pub fn parser_string_literal() {
+pub fn parse_string_literal() {
     let expected = "AST:\n[ TextNode: text: Token { kind: StringLiteral, lexeme: \"Hello, World!\", location: Location { line: 0, column: 0 } } ]\n";
     let input = "Hello, World!";
 
@@ -25,7 +25,7 @@ pub fn parser_string_literal() {
 }
 
 #[test]
-pub fn parser_write_string() {
+pub fn parse_write_string() {
     let input = "{{ write! \"Hello!\" }}";
 
     let tokens = crate::run_lexer(input);
@@ -35,8 +35,17 @@ pub fn parser_write_string() {
 }
 
 #[test]
-pub fn parser_test_simple() {
+pub fn parse_simple() {
     let input = "This is a string. {{ write! \"Hello!\" }} and another one.";
+
+    let tokens = crate::run_lexer(input);
+    let ast = crate::run_parser(tokens);
+
+    println!("{:?}", ast);
+}
+
+pub fn parse_end_stmt() {
+    let input = "{{!}}";
 
     let tokens = crate::run_lexer(input);
     let ast = crate::run_parser(tokens);
