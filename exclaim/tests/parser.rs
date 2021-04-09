8a57ca5c9d470bf297225197c24de34e8c68d9b1
jakob <Jakobpstrobl@gmail.com>
2021-04-09 15:00:38 -0400

    Tests actually test & added pretty assertions to dev-dependency

--- exclaim/tests/parser.rs
+++ exclaim/tests/parser.rs
@@ -1,53 +1,132 @@
+use pretty_assertions::{assert_eq};
 use exclaim::*;
 
 #[test]
 pub fn parse_empty_input() {
-    let expected = "AST:\n";
+    let expected = r"
+<Ast>
+</Ast>
+";
+
     let input = "";
 
     let tokens = crate::run_lexer(input);
     let ast = crate::run_parser(tokens);
 
-    assert_eq!(expected, format!("{:?}", ast));
+    assert_eq!(expected, &AstSerializer::serialize(&ast));
 }
 
 #[test]
 pub fn parse_string_literal() {
-    let expected = "AST:\n[ TextNode: text: Token { kind: StringLiteral, lexeme: \"Hello, World!\", location: Location { line: 0, column: 0 } } ]\n";
+    let expected = r#"
+<Ast>
+  <TextNode>
+    <text>
+      <Token>
+        <kind>StringLiteral</kind>
+        <lexeme>"Hello, World!"</lexeme>
+        <location>{ 0, 0 }</location>
+      </Token>
+    </text>
+  </TextNode>
+</Ast>
+"#;
     let input = "Hello, World!";
 
     let tokens = crate::run_lexer(input);
     let ast = crate::run_parser(tokens);
 
-    assert_eq!(expected, format!("{:?}", ast));
+    assert_eq!(expected, &AstSerializer::serialize(&ast));
 }
 
 #[test]
 pub fn parse_write_string() {
+    let expected = r#"
+<Ast>
+  <BlockNode>
+    <stmt>
+      <Option>
+        <StmtNode>
+          <action>
+            <Token>
+              <kind>Action(Write)</kind>
+              <lexeme>"write!"</lexeme>
+              <location>{ 0, 3 }</location>
+            </Token>
+          </action>
+          <expr>
+            <Option>
+              <LiteralExpression>
+                <literal>
+                  <Token>
+                    <kind>StringLiteral</kind>
+                    <lexeme>"Hello!"</lexeme>
+                    <location>{ 0, 10 }</location>
+                  </Token>
+                </literal>
+              </LiteralExpression>
+            </Option>
+          </expr>
+        </StmtNode>
+      </Option>
+    </stmt>
+  </BlockNode>
+</Ast>
+"#;
     let input = "{{ write! \"Hello!\" }}";
 
     let tokens = crate::run_lexer(input);
     let ast = crate::run_parser(tokens);
 
-    println!("{:?}", ast);
-}
-
-#[test]
-pub fn parse_simple() {
-    let input = "This is a string. {{ write! \"Hello!\" }} and another one.";
-
-    let tokens = crate::run_lexer(input);
-    let ast = crate::run_parser(tokens);
-
-    println!("{:?}", ast);
+    assert_eq!(expected, &AstSerializer::serialize(&ast));
 }
 
 #[test]
 pub fn parse_end_stmt() {
-    let input = "{{!}}";
+    let expected = r#"
+<Ast>
+  <TextNode>
+    <text>
+      <Token>
+        <kind>StringLiteral</kind>
+        <lexeme>"This is a string. "</lexeme>
+        <location>{ 0, 0 }</location>
+      </Token>
+    </text>
+  </TextNode>
+  <BlockNode>
+    <stmt>
+      <Option>
+        <StmtNode>
+          <action>
+            <Token>
+              <kind>Action(End)</kind>
+              <lexeme>"!"</lexeme>
+              <location>{ 0, 21 }</location>
+            </Token>
+          </action>
+          <expr>
+            <Option>None</Option>
+          </expr>
+        </StmtNode>
+      </Option>
+    </stmt>
+  </BlockNode>
+  <TextNode>
+    <text>
+      <Token>
+        <kind>StringLiteral</kind>
+        <lexeme>" and another one."</lexeme>
+        <location>{ 0, 25 }</location>
+      </Token>
+    </text>
+  </TextNode>
+</Ast>
+"#;
+    let input = "This is a string. {{ ! }} and another one.";
 
     let tokens = crate::run_lexer(input);
     let ast = crate::run_parser(tokens);
 
-    println!("{:?}", ast);
+    assert_eq!(expected, &AstSerializer::serialize(&ast));
 }
