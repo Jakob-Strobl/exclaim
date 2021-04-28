4ed986a3a8eaeedd068bc67e8bb93d1637f6a3eb
jakob <Jakobpstrobl@gmail.com>
2021-04-28 15:58:49 -0400

    Context & Scope structs and builds scope - passes both test

--- exclaim/src/semantics/semantics.rs
+++ exclaim/src/semantics/semantics.rs
@@ -1,6 +1,75 @@
+use std::fmt;
+
+const FILE_SCOPE: usize = 0;
+struct Scope {
+    level: usize, 
+    was_closed: bool,
+}
+
+impl Scope {
+    pub fn new() -> Scope {
+        Scope {
+            level: FILE_SCOPE,
+            was_closed: false,
+        }
+    }
+
+    pub fn level(&self) -> usize {
+        self.level
+    }
+
+    pub fn open(&mut self) {
+        println!("Opened scope!");
+        self.level += 1;
+    }
+
+    pub fn close(&mut self) {
+        println!("Closed scope!");
+        self.level -= 1;
+        self.was_closed = true;
+    }
+
+    pub fn was_closed(&mut self) -> bool {
+        if self.was_closed {
+            self.was_closed = false;
+            true
+        } else {
+            false
+        }
+    }
+}
+
+impl fmt::Debug for Scope {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.level == FILE_SCOPE {
+            write!(f, "File Scope")
+        } else {
+            write!(f, "Local Scope ({})", self.level)
+        }
+    }
+}
+
+struct Context {
+    scope: Scope,
+}
+
+impl Context {
+    pub fn new() -> Context {
+        Context {
+            scope: Scope::new()
+        }
+    }
+
+    pub fn scope(&mut self) -> &mut Scope {
+        &mut self.scope
+    }
+}
+
 pub mod Semantics {
     use crate::semantics::SemanticResult;
     use crate::ast::prelude::*;
+
+    use super::*;
 
     macro_rules! unwrap_element {
         ( $obj:ident.$fn:ident($idx:ident) ) => {
@@ -12,10 +81,12 @@ pub mod Semantics {
     }
 
     pub fn analyze(mut ast: Ast) -> SemanticResult<Ast> {
+        let mut ctx = Context::new();
+
         if let Some(head) = ast.head() {
             let mut current_block = head;
             loop {
-                let next_block = analyze_block(&mut ast, current_block)?;
+                let next_block = analyze_block(&mut ast, &mut ctx, current_block)?;
 
                 match next_block {
                     Some(index) => current_block = index,
@@ -27,7 +98,7 @@ pub mod Semantics {
         Ok(ast)
     }
 
-    fn analyze_block(ast: &mut Ast, block_idx: AstIndex) -> SemanticResult<Option<AstIndex>> {
+    fn analyze_block(ast: &mut Ast, ctx: &mut Context, block_idx: AstIndex) -> SemanticResult<Option<AstIndex>> {
         let element_cell = ast.get(block_idx);
 
         // Check element is a block
@@ -41,52 +112,45 @@ pub mod Semantics {
                     // Text Blocks can't fail in this context, because they are just text
                     Block::Text(_, next) => Ok(*next),
                     Block::CodeEnclosed(_, next) => {
-                        // TODO analyze the contents of the block 
+                        // TODO analyze the statement
                         Ok(*next) 
                     }
                     Block::CodeUnclosed(_, scope, next) => { 
-                        // TODO analyze the contents of the block 
+                        // TODO analyze the statement
+                        
+                        // Open Scope 
+                        ctx.scope().open();
 
-                        // Build Scope of Unclosed block
-                        if let Some(next_block) = next {
-                            let mut current_block = *next_block;
-                            loop {
-                                let element_cell = ast.get(current_block);
-                                let mut element_ref = element_cell.borrow_mut();
-                                match &mut *element_ref {
-                                    AstElement::Block(self_idx, block) => {
-                                        // TODO ? Analyze contents of block?
-
-                                        // Push the current block into the scope of the CodeUclosed block
-                                        scope.push(*self_idx);
-
-                                        match block {
-                                            // If code closing, set the next value of the Unclosed block to the closing block's next
-                                            Block::CodeClosing(_, scoped_next) => {
-                                                *next = *scoped_next;
-                                                break; // Break from scope
-                                            },
-                                            _ => {
-                                                // If not CodeClosing, set the index of current_block to the next block
-                                                current_block = match block.next() {
-                                                    Some(index) => *index,
-                                                    None => return Err("Unexpected end of AST: Expected the scope to be closed with {{!}}".to_string())
-                                                };
-                                            }
+                        // Build Scope
+                        let mut current_idx = next.unwrap(); // TODO handle unwrap properly
+                        loop {
+                            match analyze_block(ast, ctx, current_idx) {
+                                Ok(next_idx) => {
+                                    scope.push(current_idx);
+                                    if ctx.scope().was_closed() {
+                                        *next = next_idx;
+                                        break;
+                                    } else {
+                                        match next_idx {
+                                            Some(next_idx) => current_idx = next_idx,
+                                            None => return Err("Expected the scope to be closed with {{!}}".to_string()),
                                         }
-                                    },
-                                    _ => return Err("Expected a Block.".to_string())
-                                };
+                                    }
+                                },
+                                Err(e) => return Err(e),
                             }
-                        } else {
-                            // next must exist, since unclosed blocks must be closed
-                            return Err("Unclosed block's next is None. An unclosed block is expected to have a closing block".to_string());
                         }
+
                         Ok(*next)
                     }
-                    Block::CodeClosing(_, _) => {
-                        // A closing block should not exist by itself in the file scope
-                        return Err("Invalid lone closing block in file scope. Closing blocks must only be used to close an opened scope.".to_string());
+                    Block::CodeClosing(_, next) => {
+                        if ctx.scope().level() == FILE_SCOPE {
+                            // A closing block should not exist by itself in the file scope
+                            return Err("Invalid lone closing block in file scope. Closing blocks must only be used to close an opened scope.".to_string());
+                        } else {
+                            ctx.scope().close();
+                            Ok(*next)
+                        }
                     }
                 }
             }
