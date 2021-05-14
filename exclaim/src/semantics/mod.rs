10f478b7c5830562f207fbec3acb73811c397d75
jakob <Jakobpstrobl@gmail.com>
2021-05-14 01:15:24 -0400

    Style: cleanup

--- exclaim/src/semantics/mod.rs
+++ exclaim/src/semantics/mod.rs
@@ -21,12 +21,13 @@ impl SemanticContext {
     }
 }
 
-macro_rules! unwrap_element {
-    ( $obj:ident.$fn:ident($idx:ident) ) => {
-        match $obj.$fn($idx) {
-            Some(element) => element,
-            None => return Err(format!("Expected to recieve an AST Element at index {:?}", $idx)),
-        };
+macro_rules! unwrap_index {
+    ($index:expr) => {
+        if let Some(value) = $index {
+            value
+        } else {
+            return Err("Expected an AST Index.".to_string());
+        }
     };
 }
 
@@ -41,58 +42,55 @@ pub fn analyze(mut ast: Ast) -> SemanticResult<Ast> {
     Ok(ast)
 }
 
-fn analyze_block(ast: &mut Ast, ctx: &mut SemanticContext, block_idx: Option<AstIndex>) -> SemanticResult<Option<AstIndex>> {
-    if let Some(block_idx) = block_idx {
-        let element_cell = ast.get(block_idx);
-        
-        // Check element is a block
-        let mut element_ref = element_cell.borrow_mut();
-        match &mut *element_ref {
-            AstElement::Block(_, block) => { 
-                match block {
-                    // Text Blocks can't fail in this context, because they are just text
-                    Block::Text(_, next) => Ok(*next),
-                    Block::CodeEnclosed(_, next) => {
-                        // TODO analyze the statement - Not necessary for now
-                        Ok(*next) 
+fn analyze_block(ast: &mut Ast, ctx: &mut SemanticContext, block: Option<AstIndex>) -> SemanticResult<Option<AstIndex>> {
+    let block = unwrap_index!(block);
+    let block_cell = ast.get(block);
+    let mut block_ref = block_cell.borrow_mut();
+    
+    // Check element is a block
+    match &mut *block_ref {
+        AstElement::Block(_, block) => { 
+            match block {
+                // Text Blocks can't fail in this context, because they are just text
+                Block::Text(_, next) => Ok(*next),
+                Block::CodeEnclosed(_, next) => {
+                    // TODO analyze the statement - Not necessary for now
+                    Ok(*next) 
+                }
+                Block::CodeUnclosed(_, block_scope, next_block) => { 
+                    // TODO analyze the statement - Not necessary for now
+                    
+                    // Open Scope 
+                    ctx.scope().open();
+
+                    // Build the scope until it is closed
+                    let mut current_scoped_block = *next_block;
+                    while !ctx.scope().was_closed() {
+                        let next_scoped_block = match analyze_block(ast, ctx, current_scoped_block) {
+                            Ok(index) => index,
+                            Err(_) => return Err("Expected the scope to be closed with {{!}}".to_string()),
+                        };
+
+                        // analyze_block() would return an error if current_idx is None
+                        // so, we know we can unwrap current_idx
+                        block_scope.push(current_scoped_block.unwrap());
+                        current_scoped_block = next_scoped_block;
                     }
-                    Block::CodeUnclosed(_, block_scope, block_next) => { 
-                        // TODO analyze the statement - Not necessary for now
-                        
-                        // Open Scope 
-                        ctx.scope().open();
+                    *next_block = current_scoped_block;
 
-                        // Build the scope until it is closed
-                        let mut current_idx = *block_next;
-                        while !ctx.scope().was_closed() {
-                            let next_idx = match analyze_block(ast, ctx, current_idx) {
-                                Ok(idx) => idx,
-                                Err(_) => return Err("Expected the scope to be closed with {{!}}".to_string()),
-                            };
-
-                            // analyze_block() would return an error if current_idx is None
-                            // so, we know we can unwrap current_idx
-                            block_scope.push(current_idx.unwrap());
-                            current_idx = next_idx;
-                        }
-                        *block_next = current_idx;
-
-                        Ok(*block_next)
-                    }
-                    Block::CodeClosing(_, next) => {
-                        if ctx.scope().level() == scope::FILE_SCOPE {
-                            // A closing block should not exist by itself in the file scope
-                            return Err("Invalid lone closing block in file scope. Closing blocks must only be used to close an opened scope.".to_string());
-                        } else {
-                            ctx.scope().close();
-                            Ok(*next)
-                        }
+                    Ok(*next_block)
+                }
+                Block::CodeClosing(_, next) => {
+                    if ctx.scope().level() == scope::FILE_SCOPE {
+                        // A closing block should not exist by itself in the file scope
+                        return Err("Invalid lone closing block in file scope. Closing blocks must only be used to close an opened scope.".to_string());
+                    } else {
+                        ctx.scope().close();
+                        Ok(*next)
                     }
                 }
             }
-            _ => return Err("Expected a Block!".to_string()),
         }
-    } else {
-        return Err("Unexpected end of AST. Expected a block.".to_string());
+        _ => return Err("Expected a Block!".to_string()),
     }
 }
